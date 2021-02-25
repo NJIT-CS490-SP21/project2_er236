ffc1509cc70ae06c633c5315e0261ed49d6949fd
Elvis Rodriguez <elvisrodriguez0621@gmail.com>
2021-02-25 21:08:16 +0000

    fix error where boxes didnt show up correctly

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -4,15 +4,15 @@ import { useState,useRef,useEffect  } from 'react'
 
 
 export const Board=(props)=>{
-    const [board,setBoard]=useState(["_","_","_",
+    const [board,setBoard]=useState([["_","_","_",
                                     "_","_","_",
-                                    "_","_","_"])
+                                    "_","_","_"],false])
 
     useEffect(()=>{
         props.socket.on('play',(data)=>{
             console.log('Chat event received!');
             console.log(data)
-            setBoard(data['data'])
+            setBoard([data['data'],data['didWin']!="_"])
         });
         
     },[])
@@ -22,9 +22,12 @@ export const Board=(props)=>{
         props.socket.emit('play',item);
     }
     return(
-        <div className="board">
-            {board.map((items,index)=>{ return <Box  index={index}  func={send} value={board[index]} player={props.id}/>
-            })}
+        <div className="game" >
+            <div className="board">
+                {board[0].map((items,index)=>{ return <Box  index={index}  func={send} value={board[0][index]} player={props.id}/>
+                })}
+            </div>
+            {board[1] && <button>Play again</button>}
         </div>
     )
 }
